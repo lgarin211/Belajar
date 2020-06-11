94f856b14413e1e1c1bdadc3243f5a1916257386
lgarin <lagustinus211@gmail.com>
2020-06-11 20:57:03 +0700

    ini percobaan ke 2

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?= "agustinus pardaeamn lumban tobing"; ?>
+<? $a="agustinus pardaeamn lumban tobing"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>dunia satu</title>
+</head>
+
+<body>
+  <h1><?= $a; ?></h1>
+</body>
+
+</html>
